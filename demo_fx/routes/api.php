352f826f68710d0ce387aec34d5ee13a7e096a52
MD-FARHAN-UL-ASIF <farhanchowdhury66@gmail.com>
2024-03-26 17:08:21 +0600

    updated

--- demo_fx/routes/api.php
+++ demo_fx/routes/api.php
@@ -25,8 +25,7 @@ Route::controller(PostController::class)->group(function(){
     Route::get('post', 'index'); // all post list
     Route::post('create-post', 'create'); // create post
     Route::get('post/{post}', 'show'); // post by id
-    Route::get('post-by-user/{userId}', 'getPostByUserId'); //
+    Route::get('post-by-user/{userId}', 'getPostByUserId'); // get post list by userId
     Route::put('update-post/{post}','update');
     Route::delete('delete-post/{post}', 'destroy');
-
 })->middleware('auth:api');
